03d4c7d836ab5d0ef5c8cbad29ef6523c18fd4fa
Gerardo Ruiz Mendez <gerardoruizmendez012@gmail.com>
2024-04-27 17:44:51 -0600

    Deleting duplicity in CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,21 +13,6 @@ server.use(
   })
 );
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 //Router
 server.use(userRouter);
 server.use(authRouter);
