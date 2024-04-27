2cb41766e0e91a19c1439fdd42edd68960ba74f9
Gerardo Ruiz Mendez <gerardoruizmendez012@gmail.com>
2024-04-27 17:55:13 -0600

    TESTING CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,26 @@
 import express from "express";
 import userRouter from "./modules/users/application/route/users.router";
 import authRouter from "./modules/auth/application/route/auth.router";
-import cors from "cors";
 
 const server = express();
 server.set("port", 3000);
 server.use(express.json());
-server.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+
+server.use((req, res, next) => {
+  console.log("headers");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 //Router
 server.use(userRouter);
