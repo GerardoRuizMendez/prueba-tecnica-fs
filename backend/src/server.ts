97f963f7833758ddbd08c7795a8be818a69fdb43
Gerardo Ruiz Mendez <gerardoruizmendez012@gmail.com>
2024-04-27 17:23:29 -0600

    Adding cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./modules/users/application/route/users.router";
 import authRouter from "./modules/auth/application/route/auth.router";
+import cors from "cors";
 
 const server = express();
 server.set("port", 3000);
 server.use(express.json());
+server.use(cors());
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
