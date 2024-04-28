b8cd73261b420606f3d7f284a8cba5e0c344f006
Gerardo Ruiz Mendez <gerardoruizmendez012@gmail.com>
2024-04-27 18:42:50 -0600

    And another strategy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,31 +5,32 @@ import cors from "cors";
 
 const server = express();
 server.set("port", 3000);
-// server.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+server.use(
+  cors({
+    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+    exposedHeaders: ["authorization"], // you can change the headers
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
 server.use(express.json());
 
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With"
+//   );
 
-  // Allow browsers to send preflight (OPTIONS) requests without interruptions
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+//   // Allow browsers to send preflight (OPTIONS) requests without interruptions
+//   if (req.method === "OPTIONS") {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 //Router
 server.use(userRouter);
