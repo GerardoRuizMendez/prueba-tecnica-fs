c9ee4bc2d2da5c76d1b4052e4420ad6954e3a2cb
Gerardo Ruiz Mendez <gerardoruizmendez012@gmail.com>
2024-04-29 12:36:33 -0600

    adding port 0.0.0.0

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import server from "./server";
 
 function main() {
-  server.listen(server.get("port"));
+  server.listen(server.get("port"), "0.0.0.0");
   console.log("Server on port", server.get("port"));
   console.log("is this working?");
 }
